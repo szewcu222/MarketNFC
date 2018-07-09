45f3a2fa7b148396a5e51697dab012c6eabcfdaa
Damian Szewc <szewcu222@gmail.com>
2018-07-09 17:22:57 +0200

    Added Test controller for outside connections

--- MarketNFC/Controllers/HomeController.cs
+++ MarketNFC/Controllers/HomeController.cs
@@ -33,5 +33,12 @@ namespace MarketNFC.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public string Test()
+        {
+            return "test";
+        }
+
+
     }
 }
