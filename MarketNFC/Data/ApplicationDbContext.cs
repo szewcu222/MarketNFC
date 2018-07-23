d5f24bbbc00030473d84582515eba0f370a7bb36
Mateusz Strojek <mateusz.strojek@cervirobotics.com>
2018-07-23 16:46:44 +0200

    database build cfg

--- MarketNFC/Data/ApplicationDbContext.cs
+++ MarketNFC/Data/ApplicationDbContext.cs
@@ -15,6 +15,22 @@ namespace MarketNFC.Data
         {
         }
 
+        public DbSet<Grupa> Grupy { get; set; }
+        public DbSet<Produkt> Produkty { get; set; }
+        public DbSet<Lodowka> Lodowki { get; set; }
+        public DbSet<StanLodowki> StanyLodowek { get; set; }
+        public DbSet<UpodobaniaUzytkownika> UpodobaniaUzytkownikow { get; set; }
+        public DbSet<UzytkownikGrupa> UzytkownicyGrupy { get; set; }
+        public DbSet<Zamowienie> Zamowienia { get; set; }
+        public DbSet<ZamowienieProdukt> ZamowieniaProdukty { get; set; }
+
+        public static ApplicationDbContext Create()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            //optionsBuilder.UseSqlServer(connectionString, providerOptions=>providerOptions.CommandTimeout(60));
+            return new ApplicationDbContext(optionsBuilder.Options);
+        }
+
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
