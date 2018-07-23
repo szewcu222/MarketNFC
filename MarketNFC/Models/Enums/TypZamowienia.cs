8ef2e8854ac5448aab384dca0e35d2cdb4a6c88a
Mateusz Strojek <mateusz.strojek@cervirobotics.com>
2018-07-23 15:26:48 +0200

    add models version_1

---
+++ MarketNFC/Models/Enums/TypZamowienia.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MarketNFC.Models.Enums
+{
+    public enum TypeOrder { SysOrder, UserOrder };
+}
--- MarketNFC/Models/Lodowka.cs
+++ MarketNFC/Models/Lodowka.cs
@@ -1,11 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace MarketNFC.Models
 {
+    [Table("Lodowka")]
     public class Lodowka
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [ForeignKey("Grupa")]
+        public int GrupaId { get; set; }
+        [Required]
+        [Display(Name = "Pojemnosc w [L]")]
+        public int Pojemnosc { get; set; }
+        public DateTime DataAktualizacji { get; set; }
     }
 }
