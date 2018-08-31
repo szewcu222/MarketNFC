7500e12f8860566f4f18fdff00f10d9a0337818e
Mateusz Strojek <graczz.mufc@gmail.com>
2018-08-31 17:39:38 +0200

    test azure strojerk

--- MarketNFC/Controllers/HomeController.cs
+++ MarketNFC/Controllers/HomeController.cs
@@ -39,7 +39,7 @@ namespace MarketNFC.Controllers
 
             //var p = _db.Produkty.Find(1);
 
-            ViewData["Zwrotka"] = "cos";
+            ViewData["Zwrotka"] = "test";
             return View();
         }
 
