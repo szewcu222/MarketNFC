7f7598a5e5d93b7a10dfaf4c9ab86da11e776357
Mateusz Strojek <graczz.mufc@gmail.com>
2018-08-31 18:10:14 +0200

    wysw con string

--- MarketNFC/Controllers/HomeController.cs
+++ MarketNFC/Controllers/HomeController.cs
@@ -39,7 +39,7 @@ namespace MarketNFC.Controllers
 
             //var p = _db.Produkty.Find(1);
 
-            ViewData["Zwrotka"] = "test";
+            ViewData["Zwrotka"] = "nowy ConString";
             return View();
         }
 
